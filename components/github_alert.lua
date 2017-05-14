c74a0435193e9eaba893f986259e964cc0da00b6
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-05-14 13:11:19 +0800

    Cleanup code

--- components/github_alert.lua
+++ components/github_alert.lua
@@ -1,59 +1,57 @@
-local util = require 'util'
 local json = require 'json'
 local http_request = require 'http.request'
 local os = require 'os'
 local io = require 'io'
 local luadate = require 'date'
-commit = false
-commit_of_day = 0
-first_time = true
 
 require 'cairo'
 
 -- an component to remind your github little green dot :P
-local gh_endpoint = "https://api.github.com"
+
+local first_time = true
+local commit = false
+local commit_of_day = 0
 
 return function (opt)
+    local gh_endpoint = "https://api.github.com"
     local xpos = opt.x
     local ypos = opt.y + 27
     local r, g, b, a
+    local text, logo
 
-    local f = assert(io.open(opt.RESOURCE_PATH.."/../.config.json", "r"))
-    gh_cfg = json.decode(f:read("*all"))
+    local f = assert(io.open(opt.RESOURCE_PATH.."/../config.json", "r"))
+    local gh_cfg = json.decode(f:read("*all"))["github_alert"]
     f:close()
 
     -- here we check github for repo stat
     -- only do it once a minute
     --
-    today = os.date("%Y-%m-%d")
-    tick = tonumber(conky_parse("${updates}"))
+    local today = os.date("%Y-%m-%d")
+    local tick = tonumber(conky_parse("${updates}"))
 
     if first_time or tick % 150 == 0 then -- do not make request too frequently
         first_time = false
         commit_of_day = 0
-        headers, stream = assert(http_request.new_from_uri(gh_endpoint .. "/users/".. gh_cfg["gh_user"] .. "/events?access_token=" .. gh_cfg["gh_token"]):go())
-        body = stream:get_body_as_string()
-        repos = json.decode(body)
+        local _, stream = assert(http_request.new_from_uri(gh_endpoint
+        .. "/users/".. gh_cfg["gh_user"] .. "/events?access_token=" .. gh_cfg["gh_token"]):go())
+        local body = stream:get_body_as_string()
+        local repos = json.decode(body)
         -- update the results
-        for i, v in ipairs(repos) do
+        for _, v in ipairs(repos) do
             print(v["type"])
-            if v["type"] == "PushEvent" then 
-                print("HH")
-                date = v["created_at"]
+            if v["type"] == "PushEvent" then
+                local date = v["created_at"]
                 -- add 8 hours to github GMT 0 timezone
-                date_convert = luadate(date):addhours(8):fmt("%Y-%m-%d")
+                local date_convert = luadate(date):addhours(8):fmt("%Y-%m-%d")
                 if date_convert == today then
                     commit = true
                     commit_of_day = commit_of_day + tonumber(v["payload"]["size"])
                 end
             end
         end
+        -- reset the commit status if no commit today
+        if commit_of_day == 0 then commit = false end
     end
-
-    -- reset the commit status if no commit today
-    if commit_of_day == 0 then commit = false end
-    -- print(commit)
-
 
     if commit == false then
         -- Very big warning
